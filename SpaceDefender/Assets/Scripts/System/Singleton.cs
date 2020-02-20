f23316ffa2ab9a34194508e0bd8e905f5ad99777
LinsIan <jjes20055114123@gmail.com>
2020-02-20 22:22:31 +0800

    Add Singleton

---
+++ SpaceDefender/Assets/Scripts/System/Singleton.cs
@@ -0,0 +1,48 @@
+﻿//*******************************************
+// Singleton
+//*******************************************
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//*******************************************
+// Class
+//*******************************************
+abstract public class Singleton<TClass> : MonoBehaviour
+	where TClass : class
+{
+	//------------------------------------------------------
+	// Variables
+	//------------------------------------------------------
+	protected static TClass mInstance = null;
+
+	//------------------------------------------------------
+	// Accessors
+	//------------------------------------------------------
+	public static TClass Instance
+	{
+		get
+		{
+			if(mInstance == null)
+			{
+				Debug.LogError("Could not found singleton "+ typeof(TClass));
+			}
+			return mInstance;
+		}
+	}
+
+	//------------------------------------------------------
+	// Abstract Functions
+	//------------------------------------------------------
+	public abstract void Initialize();
+	
+	//------------------------------------------------------
+	// Main Functions
+	//------------------------------------------------------
+	public void SetInstance()
+	{
+		Object.DontDestroyOnLoad(this.gameObject);
+		mInstance = gameObject.GetComponent<TClass>();
+	}
+
+}
