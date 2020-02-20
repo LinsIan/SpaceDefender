3c39ad6f2732c5a03590e7c289ff2ce7b1b2d5b4
LinsIan <jjes20055114123@gmail.com>
2020-02-20 22:27:09 +0800

    Add Object Pool

---
+++ SpaceDefender/Assets/Scripts/ObjectPool/ObjectPool.cs
@@ -0,0 +1,66 @@
+﻿//*******************************************
+// Object Pooler
+//*******************************************
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+//*******************************************
+// Class
+//*******************************************
+public class ObjectPool<TClass> : MonoBehaviour
+	where TClass : MonoBehaviour
+{
+	//------------------------------------------------------
+	// Variables
+	//------------------------------------------------------
+	private GameObject    mPrefab;
+	private Queue<TClass> mClassList;
+	
+	//------------------------------------------------------
+	// Constructor
+	//------------------------------------------------------
+	public ObjectPool(GameObject iPrefab)
+	{
+		mPrefab = iPrefab;
+	}
+
+	//------------------------------------------------------
+	// Main Functions
+	//------------------------------------------------------
+	public void Initialize(GameObject iPrefab,int iInitObjectNum)
+	{
+		mPrefab = iPrefab;
+		for(int i = 0;i< iInitObjectNum; ++i)
+		{
+			Create();
+		}
+	}
+
+	public TClass Get()
+	{
+		if(mClassList.Count == 0) { Create(); }
+
+		TClass        aClass     = mClassList.Dequeue();
+		MonoBehaviour aComponent = (MonoBehaviour)aClass;
+		aComponent.transform .SetParent(null);
+		aComponent.gameObject.SetActive(true);
+		return aClass;
+	}
+	public void BackToPool(TClass iClass)
+	{
+		MonoBehaviour aComponent = (MonoBehaviour)iClass;
+		aComponent.transform .SetParent(transform);
+		aComponent.gameObject.SetActive(false);
+		mClassList.Enqueue(iClass);
+	}
+	private void Create()
+	{
+		GameObject aNewObject = Instantiate(mPrefab,transform);
+		TClass     aNewClass  = aNewObject.GetComponent<TClass>();
+		aNewObject.SetActive(false);
+		mClassList.Enqueue(aNewClass);
+	}
+	
+}
---
+++ SpaceDefender/Assets/Scripts/ObjectPool/ObjectPooler.cs
@@ -0,0 +1,83 @@
+﻿//*******************************************
+// Object Pooler
+//*******************************************
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//*******************************************
+// Class
+//*******************************************
+public class ObjectPooler : Singleton<ObjectPooler>
+{
+	//------------------------------------------------------
+	// Constants
+	//------------------------------------------------------
+	private const int INIT_BULLET_NUM = 10;
+	private const int INIT_ENEMY_NUM  = 5;
+
+	//------------------------------------------------------
+	// Variables
+	//------------------------------------------------------
+	private Dictionary<int,ObjectPool<Bullet>> mBulletPoolMap;
+	private Dictionary<int,ObjectPool<Enemy>>  mEnemyPoolMap;
+
+
+	//------------------------------------------------------
+	// Override Functions
+	//------------------------------------------------------
+	public override void Initialize(){}
+
+
+	//------------------------------------------------------
+	// Main Functions
+	//------------------------------------------------------
+	public void Clear(){}
+
+	public ObjectPool<Bullet> GetBulletPool(int iBulletID)
+	{
+		if(mBulletPoolMap.ContainsKey(iBulletID))
+		{
+			return mBulletPoolMap[iBulletID];
+		}
+		else
+		{
+			return CreateBulletPool(iBulletID);
+		}
+	}
+
+	public ObjectPool<Enemy> GetEnemyPool(int iEnemyID)
+	{
+		if(mEnemyPoolMap.ContainsKey(iEnemyID))
+		{
+			return mEnemyPoolMap[iEnemyID];
+		}
+		else
+		{
+			return CreateEnemyPool(iEnemyID);
+		}
+	}
+
+	private ObjectPool<Bullet> CreateBulletPool(int iID)
+	{
+		GameObject         aBulletPrefab = DataManager.Instance.PrefabMapsData.BulletPrefabMap[iID];
+		GameObject         aNewObject    = new GameObject();
+		ObjectPool<Bullet> aNewPool      = aNewObject.AddComponent<ObjectPool<Bullet>>();
+		aNewObject.transform.SetParent(transform);
+		aNewPool.Initialize(aBulletPrefab,INIT_BULLET_NUM);
+		mBulletPoolMap.Add(iID,aNewPool);
+		return aNewPool;
+	}
+
+	private ObjectPool<Enemy> CreateEnemyPool(int iID)
+	{
+		GameObject         aEnemyPrefab = DataManager.Instance.PrefabMapsData.EnemyPrefabMap[iID];
+		GameObject         aNewObject   = new GameObject();
+		ObjectPool<Enemy>  aNewPool     = aNewObject.AddComponent<ObjectPool<Enemy>>();
+		aNewObject.transform.SetParent(transform);
+		aNewPool.Initialize(aEnemyPrefab,INIT_ENEMY_NUM);
+		mEnemyPoolMap.Add(iID,aNewPool);
+		return aNewPool;
+	}
+
+}
