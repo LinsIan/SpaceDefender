a019ff7cc9e08d3136a8e1c06fc96d09db23c5f6
LinsIan <jjes20055114123@gmail.com>
2020-02-24 23:35:00 +0800

    Add GameOverMenu & GamePlayMenu

---
+++ SpaceDefender/Assets/Scripts/UI/GameOverMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverMenu : MonoBehaviour
+{
+	//------------------------------------------------------
+	// Main Functions
+	//------------------------------------------------------
+	public void RestartGame()
+	{
+		GameSystem.Instance.Restart();
+	}
+
+	public void ExitGame()
+	{
+		GameSystem.Instance.Exit();
+	}
+}
